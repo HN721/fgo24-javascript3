4057211a3763984e0b835702a173dbd9d7f3fb6a
Hosea Nainggolan <119390246+HN721@users.noreply.github.com>
2025-04-21 11:06:10 +0700

    Update minitask1.js

--- minitask1.js
+++ minitask1.js
@@ -1,17 +1,17 @@
 // Build Method Reduce
-// const array1 = [1, 2, 3, 4];
+const array1 = [1, 2, 3, 4];
 
-// const initialValue = 0;
-// const sumWithInitial = array1.reduce(
-//   (accumulator, currentValue) => accumulator + currentValue,
-//   initialValue
-// );
+const initialValue = 0;
+const sumWithInitial = array1.reduce(
+  (accumulator, currentValue) => accumulator + currentValue,
+  initialValue
+);
 
-// console.log(sumWithInitial);
-// // Reduce Manual
-// const array2 = [1, 2, 3, 4];
-// let initialValue2 = 0;
-// for (let i = 0; i < array2.length; i++) {
-//   initialValue2 += array2[i];
-// }
-// console.log(initialValue2);
+console.log(sumWithInitial);
+// Reduce Manual
+const array2 = [1, 2, 3, 4];
+let initialValue2 = 0;
+for (let i = 0; i < array2.length; i++) {
+  initialValue2 += array2[i];
+}
+console.log(initialValue2);
