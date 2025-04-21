5cb85c85ecd117f781010002069c289a6a7c566b
Hosea Nainggolan <119390246+HN721@users.noreply.github.com>
2025-04-21 14:04:26 +0700

    Update minitask3.js

--- minitask3.js
+++ minitask3.js
@@ -1,11 +1,8 @@
 /// Cara 1
 const animal = "ayam,bebek,sapi,unta";
-const arr = animal.split(",").with(2, "domba").join(",");
+const arr = animal.split(",").with(2, "domba").join();
 console.log(arr);
 /// Cara 2
 const animal1 = "ayam,bebek,sapi,unta";
-const arr1 = animal1
-  .split(",")
-  .map((item) => (item === "sapi" ? "domba" : item))
-  .join(",");
+const arr1 = animal1.split(",").map((item) => (item === "sapi" ? "domba" : item)).join();
 console.log(arr1);
