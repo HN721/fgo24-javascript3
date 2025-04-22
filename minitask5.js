ef0bef4c0f30210cdc29b8e515e22eb75ca26d17
Hosea Nainggolan <119390246+HN721@users.noreply.github.com>
2025-04-22 09:33:26 +0700

    Update minitask5.js

--- minitask5.js
+++ minitask5.js
@@ -1,21 +1,45 @@
+const list = [
+  {
+    name: "Jhon",
+    time: 1500,
+  },
+  {
+    name: "Ed",
+    time: 1500,
+  },
+  {
+    name: "Jane",
+    time: 1500,
+  },
+];
 function queue(nama, time) {
   return new Promise((resolve, reject) => {
-    if (nama === "Jhon" || nama === "Ed" || nama === "Jane") {
-      setTimeout(() => resolve(nama), time);
+    if (!time) {
+      reject("Harap Masukan Timer");
     } else {
-      reject("Harap Masukan Nama Yang Benar");
+      setTimeout(() => resolve(nama), time);
     }
   });
 }
-
-queue("Jhon", 1500)
-  .then((res) => {
-    console.log(res);
-    queue("Ed", 2000).then((res) => {
+const runTask = async () => {
+  try {
+    for (let i = 0; i < list.length; i++) {
+      const res = await queue(list[i].name, list[i].time);
       console.log(res);
-      queue("Jane", 500).then((res) => {
-        console.log(res);
-      });
-    });
-  })
-  .catch((err) => console.log(err));
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+runTask();
+// queue("Jhon", 1500)
+//   .then((res) => {
+//     console.log(res);
+//     queue("Ed", 2000).then((res) => {
+//       console.log(res);
+//       queue("Jane", 500).then((res) => {
+//         console.log(res);
+//       });
+//     });
+//   })
+//   .catch((err) => console.log(err));
