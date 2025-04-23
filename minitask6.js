398442d4d11e4af74cdd0cf89bc7766237b743ea
Hosea Nainggolan <119390246+HN721@users.noreply.github.com>
2025-04-23 08:24:46 +0700

    Update minitask6.js

--- minitask6.js
+++ minitask6.js
@@ -16,3 +16,34 @@ const fetchData = async () => {
   }
 };
 fetchData();
+
+const convertToLower = (str) => {
+  let result = "";
+
+  for (let i = 0; i < str.length; i++) {
+    const code = str.charCodeAt(i);
+    if (code >= 65 && code <= 90) {
+      result += String.fromCharCode(code + 32);
+    } else {
+      result += str[i];
+    }
+  }
+
+  return result;
+};
+
+const fetchEmail = () => {
+  return fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      for (let i = 0; i < data.length; i++) {
+        const email = data[i].email;
+        const lowerEmail = convertToLower(email);
+        console.log(lowerEmail);
+      }
+    })
+    .catch((err) => console.log(err));
+};
+
+fetchEmail();
+
